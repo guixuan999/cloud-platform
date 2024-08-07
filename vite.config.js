bec793f6021eb111416677120eddcf189da51799
guixuan <a20137@163.com>
2024-08-07 10:57:08 +0800

    adjust url base in vite configure

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
+  base: '/dist/',  // base defaults to '/', 果你的应用部署在子路径下，例如/app，则需要将base设置为该子路径 base: '/app/'
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
