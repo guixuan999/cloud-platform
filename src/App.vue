bcffc0d10dc8dc3647809ec6929915b0bb9a9071
guixuan <a20137@163.com>
2024-08-08 13:19:21 +0800

    add header and carousel

--- src/App.vue
+++ src/App.vue
@@ -1,86 +1,90 @@
 <script setup>
+import { ref } from 'vue'
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
+
+const micmote_logo = ref(new URL('@/assets/logo.png', import.meta.url).href);
+const carousel_pics = ref(
+  [
+    new URL('@/assets/首页1.jpg', import.meta.url).href,
+    new URL('@/assets/核心技术.jpg', import.meta.url).href,
+    new URL('@/assets/核心产品.jpg', import.meta.url).href,
+    new URL('@/assets/核心技术.jpg', import.meta.url).href
+  ]
+)
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <el-container>
+    <el-header class="header">
+      <el-row justify="space-between" style="padding: 20px 0">
+        <el-col :span="12">
+          <el-image :src="micmote_logo" style="height: 30px" />
+        </el-col>
+        <el-col :span="12">
+          <RouterLink to="/" class="custom-link">首页</RouterLink>
+          <RouterLink to="/about" class="custom-link">产品与技术</RouterLink>
+          <RouterLink to="/table" class="custom-link">客户中心</RouterLink>
+          <RouterLink to="/table" class="custom-link">尘芯新闻</RouterLink>
+          <RouterLink to="/table" class="custom-link">投资者关系</RouterLink>
+          <RouterLink to="/table" class="custom-link">关于尘芯</RouterLink>
+          <RouterLink to="/table" class="custom-link">联系我们</RouterLink>
+        </el-col>
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-        <RouterLink to="/table">Table</RouterLink>
-      </nav>
-    </div>
-  </header>
+      </el-row>
+    </el-header>
+    <el-main>
+      <el-row>
+        <el-col :span="24">
+          <el-carousel height="1100px">
+            <el-carousel-item v-for="item in carousel_pics" :key="item">
+              <el-image :src="item" />
+            </el-carousel-item>
+          </el-carousel>
+        </el-col>
+      </el-row>
+    </el-main>
+    <el-footer>Footer</el-footer>
+  </el-container>
 
-  <RouterView />
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+.custom-link {
+  padding: 0px 5px;
+  /* 上下0px，左右20px */
+  text-decoration: none;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+.el-header {
+  height: 80px;
+  background-color: #EBEEF580
 }
 
-nav {
-  width: 100%;
-  font-size: 12px;
+.el-footer {
+  height: 200px;
+  background-color: #EBEEF580
+}
+
+.demonstration {
+  color: var(--el-text-color-secondary);
+}
+
+.el-carousel__item h3 {
+  color: #475669;
+  opacity: 0.75;
+  line-height: 150px;
+  margin: 0;
   text-align: center;
-  margin-top: 2rem;
 }
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
+.el-carousel__item:nth-child(2n) {
+  background-color: #99a9bf;
 }
 
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+.el-carousel__item:nth-child(2n + 1) {
+  background-color: #d3dce6;
 }
 </style>
